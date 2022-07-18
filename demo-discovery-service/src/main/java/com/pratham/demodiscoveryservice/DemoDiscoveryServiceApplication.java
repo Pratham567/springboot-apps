c8b571ef6bc4d8c186503f35e879880b4f2b89de
Pratham Gupta <prathamgupta567@gmail.com>
2022-07-18 11:56:07 +0530

    add discovery server

---
+++ demo-discovery-service/src/main/java/com/pratham/demodiscoveryservice/DemoDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pratham.demodiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ demo-discovery-service/src/test/java/com/pratham/demodiscoveryservice/DemoDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratham.demodiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
