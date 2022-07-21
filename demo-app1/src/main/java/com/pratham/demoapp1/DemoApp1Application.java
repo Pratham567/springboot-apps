a32d04a8de89d7fed4960ff9fab05b26798e2f4b
Pratham Gupta <prathamgupta567@gmail.com>
2022-07-21 13:51:08 +0530

    enhance the flow for multiple instances of demo-1

--- demo-app1/src/main/java/com/pratham/demoapp1/DemoApp1Application.java
+++ demo-app1/src/main/java/com/pratham/demoapp1/DemoApp1Application.java
@@ -1,7 +1,9 @@
 package com.pratham.demoapp1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,15 @@ public class DemoApp1Application {
         SpringApplication.run(DemoApp1Application.class, args);
     }
 
+    @Value("${app.instance.id:0}")
+    private String instanceId;
+
+    // @Bean
+    // public String getInstanceId(){return instanceId;}
+
     @GetMapping("/")
     public String greet() {
-        return "Greetings from demo1 app!";
+        return "Greetings from demo1 app. InstanceId: " + instanceId;
     }
 
 }
--- demo-app1/src/main/java/com/pratham/demoapp1/controller/AppController.java
+++ demo-app1/src/main/java/com/pratham/demoapp1/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.pratham.demoapp1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo1")
 public class AppController {
 
+    @Value("${app.instance.id:0}")
+    private String instanceId;
+
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("Invoked demo1 hello api");
-        return "Hello from app: 1";
+        System.out.println("Invoked demo1 hello api. InstanceId: " + instanceId);
+        return "Hello from app: 1. InstanceId: " + instanceId;
     }
 
     @GetMapping("/bye")
     public String bye() {
-        return "Bye from app: 1";
+        return "Bye from app: 1. InstanceId: " + instanceId;
     }
 
 }
