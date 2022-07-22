6702956f7360f9fe95dd97caf2f90d5669e39511
Pratham Gupta <prathamgupta567@gmail.com>
2022-07-22 20:49:40 +0530

    integrate circuit-breaker-retry

--- demo-feign-client/src/main/java/com/pratham/demofeignclient/controller/AppControllerEurekaApiGateway.java
+++ demo-feign-client/src/main/java/com/pratham/demofeignclient/controller/AppControllerEurekaApiGateway.java
@@ -2,6 +2,7 @@ package com.pratham.demofeignclient.controller;
 
 import com.pratham.demofeignclient.feignclients.DemoFeignClientEurekaApiGateway;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,8 @@ public class AppControllerEurekaApiGateway {
 
     Logger logger = LoggerFactory.getLogger(AppControllerEurekaApiGateway.class);
     long count = 1;
+    long circuitBreakerFallbackCount = 1;
+    long retryFallbackCount = 1;
     private final DemoFeignClientEurekaApiGateway demoFeignClientEurekaApiGateway;
 
     public AppControllerEurekaApiGateway(DemoFeignClientEurekaApiGateway demoFeignClientEurekaApiGateway) {
@@ -27,16 +30,25 @@ public class AppControllerEurekaApiGateway {
     }
 
     @GetMapping("/demo1/hello")
-    @CircuitBreaker(name = "demo-circuitbreaker-config", fallbackMethod = "fallbackDemo1Hello")
+    @Retry(name = "demo-retry-config", fallbackMethod = "retryFallbackDemo1Hello")
+    @CircuitBreaker(name = "demo-circuitbreaker-config")
+    // @CircuitBreaker(name = "demo-circuitbreaker-config", fallbackMethod = "fallbackDemo1Hello")
     public String hello1() {
         logger.info("Count: " + count++);
         return demoFeignClientEurekaApiGateway.hello1() + " via api-gateway via Eureka via FeignClientDemo1";
     }
 
     private String fallbackDemo1Hello(Throwable th) {
-        logger.info("Inside the fallbackDemo1Hello function!!");
-        logger.error("Error: ", th);
-        return "You have got a fallback response from fallbackDemo1Hello";
+        logger.info("Inside the circuitbreaker fallbackDemo1Hello function!!");
+        logger.info("Retry Fallback count: " + retryFallbackCount++);
+        // logger.error("Error: ", th);
+        return "You have got a fallback response from circuitbreaker fallbackDemo1Hello";
+    }
+    private String retryFallbackDemo1Hello(Throwable th) {
+        logger.info("Inside the retry fallbackDemo1Hello function!!");
+        logger.info("Circuitbreaker Fallback count: " + circuitBreakerFallbackCount++);
+        // logger.error("Error: ", th);
+        return "You have got a fallback response from retry fallbackDemo1Hello";
     }
 
     @GetMapping("/demo1/bye")
