44b7af4b5e5e0e1f2a60360b5a4cd03ec0c6a57b
Pratham Gupta <prathamgupta567@gmail.com>
2022-07-22 22:57:42 +0530

    integrate sleuth (#10)

--- demo-app1/src/main/java/com/pratham/demoapp1/controller/AppController.java
+++ demo-app1/src/main/java/com/pratham/demoapp1/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.pratham.demoapp1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/demo1")
 public class AppController {
+    Logger logger = LoggerFactory.getLogger(AppController.class);
 
     @Value("${app.instance.id:0}")
     private String instanceId;
 
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("Invoked demo1 hello api. InstanceId: " + instanceId);
+        logger.info("Invoked demo1 hello api. InstanceId: " + instanceId);
         return "Hello from app: 1. InstanceId: " + instanceId;
     }
 
     @GetMapping("/bye")
     public String bye() {
+        logger.info("Invoked demo1 bye api. InstanceId: " + instanceId);
         return "Bye from app: 1. InstanceId: " + instanceId;
     }
 
