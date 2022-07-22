38430ad1082da3630a54a3c43dddb9d5ca0fe579
Pratham Gupta <prathamgupta567@gmail.com>
2022-07-22 20:52:22 +0530

    integrate circuit breaker with feign client (#2)
    * code refactor
    
    * integrate circuit-breaker

--- demo-feign-client/src/main/java/com/pratham/demofeignclient/controller/AppControllerEurekaApiGateway.java
+++ demo-feign-client/src/main/java/com/pratham/demofeignclient/controller/AppControllerEurekaApiGateway.java
@@ -1,7 +1,9 @@
 package com.pratham.demofeignclient.controller;
 
 import com.pratham.demofeignclient.feignclients.DemoFeignClientEurekaApiGateway;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/feignclient/eureka/api-gateway")
 public class AppControllerEurekaApiGateway {
 
-    @Autowired
-    private DemoFeignClientEurekaApiGateway demoFeignClientEurekaApiGateway;
+    Logger logger = LoggerFactory.getLogger(AppControllerEurekaApiGateway.class);
+    long count = 1;
+    private final DemoFeignClientEurekaApiGateway demoFeignClientEurekaApiGateway;
+
+    public AppControllerEurekaApiGateway(DemoFeignClientEurekaApiGateway demoFeignClientEurekaApiGateway) {
+        this.demoFeignClientEurekaApiGateway = demoFeignClientEurekaApiGateway;
+        logger.info("instanciating AppControllerEurekaApiGateway class");
+    }
 
     @GetMapping
     public String baseApi() {
@@ -19,8 +27,16 @@ public class AppControllerEurekaApiGateway {
     }
 
     @GetMapping("/demo1/hello")
+    @CircuitBreaker(name = "demo-circuitbreaker-config", fallbackMethod = "fallbackDemo1Hello")
     public String hello1() {
+        logger.info("Count: " + count++);
         return demoFeignClientEurekaApiGateway.hello1() + " via api-gateway via Eureka via FeignClientDemo1";
+    }
+
+    private String fallbackDemo1Hello(Throwable th) {
+        logger.info("Inside the fallbackDemo1Hello function!!");
+        logger.error("Error: ", th);
+        return "You have got a fallback response from fallbackDemo1Hello";
     }
 
     @GetMapping("/demo1/bye")
